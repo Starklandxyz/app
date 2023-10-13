2a59624a82b58230e9b4aab4a6d5f3a070b4b764
job <hujunjob@qq.com>
2023-10-13 14:54:15 +0800

    Update constants.ts

--- src/phaser/constants.ts
+++ src/phaser/constants.ts
@@ -38,5 +38,5 @@ export const TILE_WIDTH = 32;
 // contract offset so we don't overflow
 export const POSITION_OFFSET = 1000;
 
-export const MAP_WIDTH = 50
-export const MAP_HEIGHT = 50
+export const MAP_WIDTH = 100
+export const MAP_HEIGHT = 100
